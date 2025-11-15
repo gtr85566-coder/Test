dc82498404bfb60f9788209e4f0ba4b4a6bfc81e
VagrrScriptzz <gtr85566@gmail.com>
2025-11-15 22:29:32 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,9 +1,1 @@
-import telebot
-
-bot = telebot.TeleBot("8121194143:AAG8iBrkjbnKQyv1ogxDjji_Y1TNJ5ojecw")
-
-@bot.message_handler(content_types=['text'])
-def echo(message):
-    bot.send_message(message.chat.id, text=f"{message.text}")
-	
-bot.polling()
+ej
